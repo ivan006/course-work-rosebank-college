05d8d045bdb234ee759fbbdf9aebcbf2e32b02c6
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-03-15 15:59:01 +0000

    1

--- Act_2_4_1/app/src/main/java/com/example/act_2_4_1/MainActivity.kt
+++ Act_2_4_1/app/src/main/java/com/example/act_2_4_1/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.act_2_4_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton)
+
+        val welcomeTextView = findViewById<TextView>(R.id.welcomeTextView)
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        clickMeButton?.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Button Clicked", Toast.LENGTH_LONG).show()
+            welcomeTextView.text = "Welcome, ${nameEditText.text}"
+        }
     }
 }
