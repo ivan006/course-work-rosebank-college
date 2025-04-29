fa81b0c8bd770babc085e3109f6e04ef310b8ace
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-29 13:49:20 +0000

    1

--- 4_2_mm/app/src/main/java/com/example/a4_2_mm/MultiplicationTable.kt
+++ 4_2_mm/app/src/main/java/com/example/a4_2_mm/MultiplicationTable.kt
@@ -14,11 +14,24 @@ class MultiplicationTable : AppCompatActivity() {
         val tableString: String? = bundle?.getString("tableNumber")
 
         // convert the table number to an integer
-        val tableNumber = tableString?.toInt()
+        val tableNumber = tableString!!.toInt()
 
         // display the heading so we can see the value in action
         val multiplicationTable = findViewById<TextView>(R.id.multiplicationTableTextView)
 
-        multiplicationTable.text = "$tableNumber x table"
+        // start with the heading and add two new lines
+        var tableDisplay: String = "$tableNumber x table\n\n"
+
+        // declare the control variable
+        var counter = 1
+        while (counter <= 10) { // check the control variable
+            val answer = tableNumber * counter
+            tableDisplay += "$tableNumber x $counter = ${answer}\n"
+            counter++ // update the control variable
+        }
+
+        // set the string onto the display
+        multiplicationTable.text = tableDisplay
+
     }
 }
