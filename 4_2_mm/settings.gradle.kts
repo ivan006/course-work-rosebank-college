88961daaf650aae44b60b12275eed4a79d1f69ea
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-29 11:12:59 +0000

    1

---
+++ 4_2_mm/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "4_2_mm"
+include(":app")
+ 
