ccf4af1a435b7b1db84a5a294ff4a1bfab249b57
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-29 12:02:27 +0000

    1

--- 4_2_mm/app/src/main/java/com/example/a4_2_mm/MainActivity.kt
+++ 4_2_mm/app/src/main/java/com/example/a4_2_mm/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.a4_2_mm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val clickMeButton = findViewById<Button>(R.id.button)
+
+
+        val welcomeTextView = findViewById<TextView>(R.id.textView)
+        val nameEditText = findViewById<EditText>(R.id.editTextText)
+
+
+        clickMeButton?.setOnClickListener {
+            var greeting: String = ""
+
+
+            greeting = "Welcome, ${nameEditText.text}"
+            welcomeTextView.text = greeting
+
+
+
+
+        }
     }
 }
