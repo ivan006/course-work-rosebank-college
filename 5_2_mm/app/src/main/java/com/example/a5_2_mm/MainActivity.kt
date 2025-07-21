b85275cae8a97b5438dfb37f8a41fa2bd27ad8d0
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-07-21 11:14:57 +0200

    1

--- 5_2_mm/app/src/main/java/com/example/a5_2_mm/MainActivity.kt
+++ 5_2_mm/app/src/main/java/com/example/a5_2_mm/MainActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    // Teams from https://footballdatabase.com/ranking/south-africa/1
+    // retrieved on 21 May 2022
+    val teams = arrayOf<String>("Mamelodi Sundowns FC",
+        "Bidvest Wits",
+        "Orlando Pirates",
+        "Kaizer Chiefs",
+        "Cape Town City FC")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
+
